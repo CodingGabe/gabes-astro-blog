f01e3d02f60b9cde9082fd1269dc2deabf37beca
Gabe Amaya <gabeamaya2@gmail.com>
2023-11-16 11:58:40 -0600

    post page mobile fix, add date for post. add tranistion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx(), react()]
 });
